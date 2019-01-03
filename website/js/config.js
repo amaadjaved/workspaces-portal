2101c48e025ae5797d0d2e5b61edeb4dea5f41f5
amaadjaved <a.javee@gmail.com>
2019-01-04 01:45:53 +0500

    Update config.js

--- website/js/config.js
+++ website/js/config.js
@@ -1,13 +1,13 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_92k873Hri', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '3ln7jmh7as7be6std6jjv8kri9', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-1_Fd9nFh8Fl', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '2fe8a73uv51gafu8pdpeilkt54', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
-        invokeUrl: 'https://vhy99yf32f.execute-api.us-east-1.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: ' https://30in16toz6.execute-api.us-east-1.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     },
     approval: {
-        email: 'ajaved@ztechstudio.com'
+        email: 'ceo@mmobitechinc.net'
     }
 };
