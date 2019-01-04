9c0b9608da590a1b7af2b04b1460ad73e158cdca
amaadjaved <a.javee@gmail.com>
2019-01-04 22:04:40 +0500

    Update config.js

--- website/js/config.js
+++ website/js/config.js
@@ -5,7 +5,7 @@ window._config = {
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
-        invokeUrl: ' https://30in16toz6.execute-api.us-east-1.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://30in16toz6.execute-api.us-east-1.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     },
     approval: {
         email: 'approval@mobitechpro.net'
