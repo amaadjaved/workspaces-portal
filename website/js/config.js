8f3ac625c4d9d0f529539535c5bffb339112a1b2
amaadjaved <a.javee@gmail.com>
2019-01-05 04:00:47 +0500

    Update config.js

--- website/js/config.js
+++ website/js/config.js
@@ -1,13 +1,13 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_Fd9nFh8Fl', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '2fe8a73uv51gafu8pdpeilkt54', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: 'us-east-1' // e.g. us-east-2
+        userPoolId: 'us-west-2_LeR92C5jx', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '5ssk755qu27keqfurevu1ak7jn', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-west-2' // e.g. us-east-2
     },
     api: {
-        invokeUrl: 'https://30in16toz6.execute-api.us-east-1.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://2v6a6530u2.execute-api.us-west-2.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     },
     approval: {
-        email: 'approval@mobitechpro.net'
+        email: 'user1@mobitechpro.net'
     }
 };
